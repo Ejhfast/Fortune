bbe5d785b9ae2284438809944bf121cf11899b6b
Ethan Fast <ethan@ethan-fasts-macbook-pro.local>
2009-12-30 12:54:17 -0500

    style

--- run.rb
+++ run.rb
@@ -5,6 +5,19 @@ template :index do
 	'= fortune'
 end
 
+error NumberNotExist do
+	'Not a valid request'
+end
+
+get '/:number/json' do
+	num = params[:number].to_i
+	if num < 0 or num > 10
+		raise NumberNotExist
+	else
+		myfs = (0..num).map{|x| { 'number' => x, 'fortune' => `fortune`} }
+	end
+end	
+
 get '/*' do
 	haml :index, :locals => { :fortune => `fortune` }
 end
